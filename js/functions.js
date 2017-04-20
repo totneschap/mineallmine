0e672f1f543571f674e23ff8fdd76113cb068bd5
Shane Whittaker <shanewhittaker@Shanes-iMac.local>
2017-04-20 22:11:56 +0100

    Add existing file

--- js/functions.js
+++ js/functions.js
@@ -20,33 +20,33 @@ $('document').ready(function() {
 });
 
 
-$(function(){
-  window.app = {};
-  window.app.$main_top = $('.main-image');
-  window.app.main_top_height = window.app.$main_top.outerHeight();
-  window.app.flips = [];
-  $('.flip').each(function(index,item){
-      var $flip = $(item);
-      window.app.flips.push({
-        'flip'   : $flip,
-        'height' : $flip.outerHeight(),
-        'width' : $flip.outerWidth(),
-        'position' : $flip.position()
-      });
-  })
-});
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var currentBottom = window.app.main_top_height - scroll;
-
-    if (currentBottom < 0) return;
-
-    window.app.flips.forEach(function(flip){
-        if (currentBottom <= ((flip.width / 0.275) + flip.position.top)){
-          flip.flip.addClass('scrolling');
-        }else{
-          flip.flip.removeClass('scrolling');
-        }
-    });
-});
+// $(function(){
+//   window.app = {};
+//   window.app.$main_top = $('.main-image');
+//   window.app.main_top_height = window.app.$main_top.outerHeight();
+//   window.app.flips = [];
+//   $('.flip').each(function(index,item){
+//       var $flip = $(item);
+//       window.app.flips.push({
+//         'flip'   : $flip,
+//         'height' : $flip.outerHeight(),
+//         'width' : $flip.outerWidth(),
+//         'position' : $flip.position()
+//       });
+//   })
+// });
+//
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//     var currentBottom = window.app.main_top_height - scroll;
+//
+//     if (currentBottom < 0) return;
+//
+//     window.app.flips.forEach(function(flip){
+//         if (currentBottom <= ((flip.width / 0.275) + flip.position.top)){
+//           flip.flip.addClass('scrolling');
+//         }else{
+//           flip.flip.removeClass('scrolling');
+//         }
+//     });
+// });
